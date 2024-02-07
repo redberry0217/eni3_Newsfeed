59c7a8ecce6eec9d7ecb14ca262935cf906e7af8
redberry0217 <redberry0217@gmail.com>
2024-02-07 20:48:56 +0900

    project: Layout Header 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,3 +1,4 @@
+import Layout from '../components/Layout';
 import Auth from 'pages/Auth';
 import CodeSubmit from 'pages/CodeSubmit';
 import Detail from 'pages/Detail';
@@ -9,11 +10,13 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/submit" element={<CodeSubmit />} />
-        <Route path="/auth" element={<Auth />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/submit" element={<CodeSubmit />} />
+          <Route path="/auth" element={<Auth />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
