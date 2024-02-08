35aa3f94101add9600131675cd8ab1d7804657d5
lauichan <te06230623@gmail.com>
2024-02-08 10:36:21 +0900

    feat : 글 목록 프로필 이미지 추가

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,18 +1,33 @@
+import { userList } from 'static/user';
 import styled from 'styled-components';
+
+function ArticleItem({ article }) {
+  const { authorId, title, createdAt, content, like } = article;
+  const { nickname, avatar } = userList.find((user) => user.id === authorId);
+
+  return (
+    <Article>
+      <Author>
+        <Avatar src={avatar} alt={nickname} />
+        <NickName>{nickname}</NickName>
+      </Author>
+      <ContentWrap>
+        <Title>{title}</Title>
+        <Content>{content}</Content>
+        <Bottom>
+          <time>{new Date(createdAt).toLocaleString('ko-KR', { timeZone: 'UTC' })}</time>
+          <span>{like}</span>
+        </Bottom>
+      </ContentWrap>
+    </Article>
+  );
+}
 
 function ArticleList({ list }) {
   return (
     <Section>
       {list.map((article) => {
-        return (
-          <Article key={article.id}>
-            {article.author}
-            <Title>{article.title}</Title>
-            <time>{new Date(article.createdAt).toLocaleString('ko-KR', { timeZone: 'UTC' })}</time>
-            <p>{article.content}</p>
-            <span>{article.like}</span>
-          </Article>
-        );
+        return <ArticleItem key={article.id} article={article} />;
       })}
     </Section>
   );
@@ -27,9 +42,8 @@ const Section = styled.section`
 
 const Article = styled.article`
   display: flex;
-  flex-direction: column;
   gap: 1rem;
-  padding: 1rem;
+  padding: 2rem;
   background-color: #f2f2f2;
   border-radius: 10px;
 `;
@@ -37,6 +51,43 @@ const Article = styled.article`
 const Title = styled.h2`
   font-size: 1.3rem;
   font-weight: bold;
+`;
+
+const Author = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const Avatar = styled.img`
+  width: 4rem;
+  height: 4rem;
+  border-radius: 50%;
+`;
+
+const NickName = styled.span`
+  font-weight: bold;
+`;
+
+const ContentWrap = styled.div`
+  flex: 1 1 0;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: auto;
+`;
+
+const Content = styled.p`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 export default ArticleList;
--- src/static/data.js
+++ src/static/data.js
@@ -4,16 +4,18 @@ export const articleList = [
     createdAt: '2023-11-03T02:07:09.423Z',
     authorId: 1,
     title: '제목',
-    content: '한마디',
+    content:
+      '가나다라마바사아자차가나다라마바사아자차가나다라마바사아자차가나다라마바사아자차가나다라마바사아자차가나다라마바사아자차가나다라마바사아자차가나다라마바사아자차가나다라마바사아자차',
     code: 'console.log(123123)',
     like: 4
   },
   {
     id: 2,
     createdAt: '2023-11-02T23:13:18.491Z',
-    authorId: 1,
+    authorId: 4,
     title: '제목',
-    content: '한마디',
+    content:
+      'abcdefghijkasdfasdfasfasabcdefghijkasdfasdfasfasabcdefghijkasdfasdfasfasabcdefghijkasdfasdfasfasabcdefghijkasdfasdfasfasabcdefghijkasdfasdfasfasabcdefghijkasdfasdfasfasabcdefghijkasdfasdfasfasabcdefghijkasdfasdfasfas',
     code: 'console.log(123123)',
     like: 20
   },
@@ -22,7 +24,8 @@ export const articleList = [
     createdAt: '2023-11-02T11:25:37.026Z',
     authorId: 2,
     title: '제목',
-    content: '한마디',
+    content:
+      '한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디한마디',
     code: 'console.log(123123)',
     like: 30
   },
@@ -31,7 +34,8 @@ export const articleList = [
     createdAt: '2023-11-02T16:06:34.150Z',
     authorId: 2,
     title: '제목',
-    content: '한마디',
+    content:
+      'ㅁㄴㅇㄹ;ㅣ마넝ㄹ;민ㅇ라;민ㅇ라ㅓ;ㅁ닝라ㅓㅁㄴㅇㄹ민ㅇ라ㅁㄴㅇㄹ;ㅣ마넝ㄹ;민ㅇ라;민ㅇ라ㅓ;ㅁ닝라ㅓㅁㄴㅇㄹ민ㅇ라',
     code: 'console.log(123123)',
     like: 52
   },
@@ -40,7 +44,7 @@ export const articleList = [
     createdAt: '2023-11-03T05:40:17.575Z',
     authorId: 3,
     title: '제목',
-    content: '한마디',
+    content: 'hello world',
     code: 'console.log(123123)',
     like: 10
   }
