495eca1ff8e1f82b4ff3af8a44b3d278b52c0159
ugyobppang <yeonguk0517@gmail.com>
2024-02-08 12:28:17 +0900

    feat : CodeSubmit page 기본 구조 및 스타일링 세팅

---
+++ src/components/CodeKataForm.jsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function CodeKataForm() {
+  return (
+    <FormArea>
+      <InputArea>
+        <label>제목</label>
+        <input type="text" name="title" placeholder="해결한 문제 제목을 입력해주세요." />
+      </InputArea>
+      <InputArea>
+        <label>한마디</label>
+        <input type="text" name="content" placeholder="해결한 문제에 대한 평을 입력해주세요." />
+      </InputArea>
+      <InputArea>
+        <label>주소</label>
+        <input type="text" name="link" placeholder="해결한 문제 주소를 입력해주세요." />
+      </InputArea>
+      <InputArea>
+        <label>체감 난이도</label>
+        <select name="difficulty">
+          <option value="">별점을 선택해주세요.</option>
+          <option value="⭐">⭐</option>
+          <option value="⭐⭐">⭐⭐</option>
+          <option value="⭐⭐⭐">⭐⭐⭐</option>
+          <option value="⭐⭐⭐⭐">⭐⭐⭐⭐</option>
+          <option value="⭐⭐⭐⭐⭐">⭐⭐⭐⭐⭐</option>
+        </select>
+      </InputArea>
+      <InputArea>
+        <label>코드</label>
+        <textarea name="code" placeholder="해결한 문제 코드를 입력해주세요."></textarea>
+      </InputArea>
+      <button>등록하기</button>
+    </FormArea>
+  );
+}
+
+export default CodeKataForm;
+
+const FormArea = styled.form`
+  background-color: #ececec;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 2rem;
+  border-radius: 8px;
+  & button {
+    width: 180px;
+    height: 40px;
+    border: none;
+    border-radius: 8px;
+    background-color: #2f89d1;
+    color: white;
+    margin-left: auto;
+    cursor: pointer;
+    font-weight: 600;
+    font-size: 14pt;
+  }
+  & button:active {
+    background-color: #0b65ad;
+  }
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  & label {
+    padding-left: 6px;
+  }
+  & input,
+  select,
+  textarea {
+    padding: 5px 8px;
+    border: 2px solid #c7c7c7;
+    border-radius: 8px;
+  }
+  & select {
+    width: 200px;
+  }
+  & textarea {
+    resize: none;
+    height: 100px;
+  }
+`;
--- src/pages/CodeSubmit.jsx
+++ src/pages/CodeSubmit.jsx
@@ -1,9 +1,37 @@
+import CodeKataForm from 'components/CodeKataForm';
+import styled from 'styled-components';
+
 function CodeSubmit() {
   return (
-    <>
-      <div>CodeSubmit</div>
-    </>
+    <Container>
+      <TitleArea>
+        <h1>코드카타</h1>
+        <p>반복되는 연습이라는 뜻을 가진 'Kata'를 통해 매일 코딩을 훈련해봅시다.</p>
+      </TitleArea>
+      <CodeKataForm />
+    </Container>
   );
 }
 
 export default CodeSubmit;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 45px;
+`;
+
+const TitleArea = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  & h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    color: #2f89d1;
+  }
+`;
