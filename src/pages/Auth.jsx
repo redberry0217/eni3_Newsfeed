8245e6ae94cf75397d8258347689764bf588b1e3
kewii33 <kewiii071@gmail.com>
2024-02-08 19:51:10 +0900

    style : 로그인, 회원가입 UI 업데이트

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { auth, setGithubLogin, setGooGleLogin } from '../firebase';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { FcGoogle } from 'react-icons/fc';
+import { ImGithub } from 'react-icons/im';
+import { BsBoxArrowInRight } from 'react-icons/bs';
+import styled from 'styled-components';
 
-const Auth = () => {
+function Auth() {
+  const navigate = useNavigate();
+  const ClickToSignUpPageHandler = () => {
+    navigate('/signup');
+  };
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -18,15 +28,6 @@ const Auth = () => {
     }
   };
 
-  const signUp = async (event) => {
-    event.preventDefault();
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   const signIn = async (event) => {
     event.preventDefault();
     try {
@@ -36,32 +37,171 @@ const Auth = () => {
     }
   };
 
-  const logOut = async (event) => {
-    event.preventDefault();
+  // const logOut = async (event) => {
+  //   event.preventDefault();
 
-    await signOut(auth);
-  };
+  //   await signOut(auth);
+  // };
 
   return (
-    <div>
-      <h2>로그인</h2>
-      <form>
-        <div>
-          <label>이메일 : </label>
-          <input type="email" value={email} name="email" onChange={onChange} required></input>
-        </div>
-        <div>
-          <label>비밀번호 : </label>
-          <input type="password" value={password} name="password" onChange={onChange} required></input>
-        </div>
-        <button onClick={signUp}>회원가입</button>
-        <button onClick={signIn}>로그인</button>
-        <button onClick={logOut}>로그아웃</button>
-      </form>
-      <button onClick={setGooGleLogin}>Google 로그인</button>
-      <button onClick={setGithubLogin}>Git 로그인</button>
-    </div>
+    <LoginPage>
+      <LoginBox>
+        <LoginTitle>LOGIN</LoginTitle>
+        <LoginComment>
+          E&I3에서 개발자의 꿈에 <br />한 걸음 더 다가가세요!
+        </LoginComment>
+        <form>
+          <InputAreas>
+            <InputEmail
+              type="email"
+              value={email}
+              placeholder="이메일"
+              name="email"
+              onChange={onChange}
+              required
+            ></InputEmail>
+            <InputPassword
+              type="password"
+              value={password}
+              placeholder="비밀번호"
+              name="password"
+              onChange={onChange}
+              required
+            ></InputPassword>
+          </InputAreas>
+          <LoginBtn onClick={signIn}>로그인</LoginBtn>
+          {/* <button onClick={logOut}>로그아웃</button> */}
+        </form>
+      </LoginBox>
+      <BottomOfLoginPage>
+        <SocialLoginBtns>
+          <p>소셜 계정으로 로그인</p>
+          <StyledFcGoogle onClick={setGooGleLogin}></StyledFcGoogle>
+          <StyledImGithub onClick={setGithubLogin}></StyledImGithub>
+        </SocialLoginBtns>
+        <SignUpIcon onClick={ClickToSignUpPageHandler}>
+          <StyledBsBoxArrowInRight />
+          <p>회원가입</p>
+        </SignUpIcon>
+      </BottomOfLoginPage>
+    </LoginPage>
   );
-};
+}
+
+const LoginPage = styled.div`
+  height: 75vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const LoginBox = styled.div`
+  width: 800px;
+  height: 450px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #f2f2f2;
+  border-radius: 20px;
+  gap: 20px;
+`;
+
+const LoginTitle = styled.div`
+  color: #2f89d1;
+  font-size: 30pt;
+`;
+
+const LoginComment = styled.div`
+  font-size: 15pt;
+  line-height: 1.3;
+`;
+
+const InputAreas = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const InputEmail = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const InputPassword = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const LoginBtn = styled.button`
+  width: 300px;
+  height: 50px;
+  margin-top: 40px;
+  border-radius: 20px;
+  color: white;
+  background-color: black;
+  font-size: 18pt;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #0b65ad;
+    transition: background-color 0.4s ease;
+  }
+`;
+
+const BottomOfLoginPage = styled.div`
+  width: 750px;
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const SocialLoginBtns = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 15pt;
+  gap: 15px;
+`;
+
+const StyledFcGoogle = styled(FcGoogle)`
+  width: 40px;
+  height: 40px;
+  background-color: transparent;
+  border-radius: 100%;
+  cursor: pointer;
+`;
+
+const StyledImGithub = styled(ImGithub)`
+  width: 40px;
+  height: 40px;
+  background-color: transparent;
+  border-radius: 100%;
+  cursor: pointer;
+`;
+
+const SignUpIcon = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 15pt;
+  gap: 10px;
+  cursor: pointer;
+`;
+
+const StyledBsBoxArrowInRight = styled(BsBoxArrowInRight)`
+  width: 40px;
+  height: 40px;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
 export default Auth;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,414 @@
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, setGithubLogin, setGooGleLogin } from '../firebase';
+import { FcGoogle } from 'react-icons/fc';
+import { ImGithub } from 'react-icons/im';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+function SignUp() {
+  const [email, setEmail] = useState('');
+  const [customDomain, setCustomDomain] = useState('');
+  const [selectedDomain, setSelectedDomain] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [icon, setIcon] = useState('');
+  const [status, setStatus] = useState('');
+
+  const navigate = useNavigate();
+
+  const CancelSignUpHandler = () => {
+    const cancelConfirm = window.confirm('가입을 취소하시겠습니까?');
+    if (cancelConfirm) {
+      navigate('/');
+    } else {
+      return;
+    }
+  };
+
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'customDomain') {
+      setCustomDomain(value);
+    }
+  };
+
+  const onSelectChange = (event) => {
+    const selectedOption = event.target.value;
+    setSelectedDomain(selectedOption);
+    if (selectedOption === '직접 쓰기') {
+      setCustomDomain('');
+    } else {
+      setCustomDomain(selectedOption);
+    }
+  };
+
+  const signUp = async (event) => {
+    event.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SignUpPage>
+      <SignUpBox>
+        <SignUpInputBox>
+          <SignUpInputsTitle>회원가입</SignUpInputsTitle>
+          <SignUpInputs>
+            <div>
+              <InputEmailTitle>이메일(아이디)*</InputEmailTitle>
+              <InputEmailBox>
+                <InputEmail type="email" name="email" value={email} onChange={onChange} required></InputEmail>
+                <StyledAtSymbol>@</StyledAtSymbol>
+                <InputEmailDomain
+                  name="customDomain"
+                  value={customDomain}
+                  placeholder="example.com"
+                  disabled={selectedDomain !== '직접 쓰기'}
+                  onChange={onChange}
+                ></InputEmailDomain>
+                <StyledSelectDomain onChange={onSelectChange}>
+                  <option value="" disabled selected hidden>
+                    선택해주세요
+                  </option>
+                  <option>naver.com</option>
+                  <option>gmail.com</option>
+                  <option>hanmail.com</option>
+                  <option>nate.com</option>
+                  <option>직접 쓰기</option>
+                </StyledSelectDomain>
+                <CheckDuplicateEmailBtn>중복 확인</CheckDuplicateEmailBtn>
+              </InputEmailBox>
+            </div>
+            <InputPasswordBox>
+              <div>
+                <InputPasswordTitle>비밀번호*</InputPasswordTitle>
+                <InputPassword
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></InputPassword>
+              </div>
+              <div>
+                <InputConfirmPasswordTitle>비밀번호 확인*</InputConfirmPasswordTitle>
+                <InputConfirmPassword
+                  type="password"
+                  name="password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                ></InputConfirmPassword>
+              </div>
+            </InputPasswordBox>
+            <NickNameAndIconSection>
+              <div>
+                <NickNameTitle>닉네임*</NickNameTitle>
+                <InputNickName
+                  type="text"
+                  name="nickname"
+                  value={nickname}
+                  onChange={(e) => setNickname(e.target.value)}
+                  required
+                ></InputNickName>
+              </div>
+              <div>
+                <IconTitle>아이콘*</IconTitle>
+                <StyledSelectIcon name="icon" value={icon} onChange={(e) => setIcon(e.target.value)} required>
+                  <option>개발하는 고양이</option>
+                  <option>개발하는 강아지</option>
+                  <option>개발하는 햄스터</option>
+                  <option>개발하는 병아리</option>
+                </StyledSelectIcon>
+              </div>
+            </NickNameAndIconSection>
+            <div>
+              <MyStatusTitle>나의 현재 상태</MyStatusTitle>
+              <StyledSelectMyStatus name="status" value={status} onChange={(e) => setStatus(e.target.value)} required>
+                <option value="" disabled selected hidden>
+                  선택해주세요
+                </option>
+                <option>주니어 개발자</option>
+                <option>시니어 개발자</option>
+                <option>프리랜서</option>
+                <option>학생</option>
+              </StyledSelectMyStatus>
+            </div>
+          </SignUpInputs>
+        </SignUpInputBox>
+        <SignUpBtns>
+          <SocialSignUpBtns>
+            <p>소셜 계정으로 가입</p>
+            <StyledFcGoogle onClick={setGooGleLogin}></StyledFcGoogle>
+            <StyledImGithub onClick={setGithubLogin}></StyledImGithub>
+          </SocialSignUpBtns>
+          <SignUpOptionBtns>
+            <ConfirmSignUpBtn onClick={signUp}>가입하기</ConfirmSignUpBtn>
+            <CancelSignUpBtn onClick={CancelSignUpHandler}>취소하기</CancelSignUpBtn>
+          </SignUpOptionBtns>
+        </SignUpBtns>
+      </SignUpBox>
+    </SignUpPage>
+  );
+}
+
+const SignUpPage = styled.div`
+  height: 75vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const SignUpInputsTitle = styled.div`
+  display: flex;
+  font-size: 35px;
+  color: #2f89d1;
+  padding: 15px;
+  border-bottom: solid 2px #c7c7c7;
+`;
+
+const SignUpBox = styled.div`
+  width: 1300px;
+  height: 600px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const SignUpInputBox = styled.div`
+  width: 1300px;
+  height: 550px;
+  display: flex;
+  flex-direction: column;
+  border-bottom: solid 2px #c7c7c7;
+`;
+
+const SignUpInputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  gap: 30px;
+`;
+
+const InputEmailBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const InputEmailTitle = styled.div`
+  font-size: 15pt;
+  margin-left: 8px;
+  margin-bottom: 8px;
+`;
+
+const InputEmail = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const InputEmailDomain = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const StyledAtSymbol = styled.div`
+  font-size: 20pt;
+  margin-left: 3px;
+  margin-right: 3px;
+`;
+
+const StyledSelectDomain = styled.select`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const CheckDuplicateEmailBtn = styled.button`
+  width: 200px;
+  height: 50px;
+  margin-left: 10px;
+  border-radius: 20px;
+  color: white;
+  background-color: black;
+  font-size: 18pt;
+  border: none;
+  cursor: pointer;
+`;
+
+const InputPasswordBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+`;
+
+const InputPasswordTitle = styled.div`
+  font-size: 15pt;
+  margin-left: 8px;
+  margin-bottom: 8px;
+`;
+
+const InputPassword = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const InputConfirmPasswordTitle = styled.div`
+  font-size: 15pt;
+  margin-left: 8px;
+  margin-bottom: 8px;
+`;
+
+const InputConfirmPassword = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const NickNameAndIconSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+`;
+
+const NickNameTitle = styled.div`
+  font-size: 15pt;
+  margin-left: 8px;
+  margin-bottom: 8px;
+`;
+
+const InputNickName = styled.input`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const IconTitle = styled.div`
+  font-size: 15pt;
+  margin-left: 8px;
+  margin-bottom: 8px;
+`;
+
+const StyledSelectIcon = styled.select`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const MyStatusTitle = styled.div`
+  font-size: 15pt;
+  margin-left: 8px;
+  margin-bottom: 8px;
+`;
+
+const StyledSelectMyStatus = styled.select`
+  width: 300px;
+  height: 45px;
+  border: 2px solid #5f5f5f;
+  border-radius: 20px;
+  padding: 10px 20px;
+  font-size: 15pt;
+`;
+
+const SocialSignUpBtns = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 15pt;
+  gap: 15px;
+`;
+
+const StyledFcGoogle = styled(FcGoogle)`
+  width: 40px;
+  height: 40px;
+  background-color: transparent;
+  border-radius: 100%;
+  cursor: pointer;
+`;
+
+const StyledImGithub = styled(ImGithub)`
+  width: 40px;
+  height: 40px;
+  background-color: transparent;
+  border-radius: 100%;
+  cursor: pointer;
+`;
+
+const SignUpBtns = styled.div`
+  width: 1300px;
+  height: 50px;
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const SignUpOptionBtns = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const ConfirmSignUpBtn = styled.button`
+  width: 230px;
+  height: 50px;
+  margin-left: 10px;
+  border-radius: 20px;
+  color: white;
+  background-color: black;
+  font-size: 18pt;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #0b65ad;
+    transition: background-color 0.4s ease;
+  }
+`;
+
+const CancelSignUpBtn = styled.button`
+  width: 230px;
+  height: 50px;
+  margin-left: 10px;
+  border-radius: 20px;
+  color: white;
+  background-color: #929292;
+  font-size: 18pt;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #0b65ad;
+    transition: background-color 0.4s ease;
+  }
+`;
+
+export default SignUp;
