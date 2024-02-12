527ab0c077bb5d219ab5bd18262946f7e8634cd7
lauichan <te06230623@gmail.com>
2024-02-12 12:01:19 +0900

    feat: redux 유저 데이터

--- src/store/config/configStore.js
+++ src/store/config/configStore.js
@@ -3,8 +3,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import article from 'store/modules/article';
 import comment from 'store/modules/comment';
 import loginAccess from 'store/modules/loginAccess';
+import users from 'store/modules/users';
 
-const rootReducer = combineReducers({ article, comment, loginAccess });
+const rootReducer = combineReducers({ article, comment, users, loginAccess });
 const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
