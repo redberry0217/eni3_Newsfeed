94dca11a86bdede9a9e5e16ce2cfb73b195b0b93
lauichan <te06230623@gmail.com>
2024-02-14 15:02:05 +0900

    feat: 게시글 수정 헤더

--- src/pages/CodeSubmit.jsx
+++ src/pages/CodeSubmit.jsx
@@ -43,10 +43,6 @@ function CodeSubmit() {
 
   return (
     <Container>
-      <TitleArea>
-        <h1>코드카타</h1>
-        <p>반복되는 연습이라는 뜻을 가진 'Kata'를 통해 매일 코딩을 훈련해봅시다.</p>
-      </TitleArea>
       <CodeKataForm onSubmitHandler={onSubmitHandler} />
     </Container>
   );
@@ -63,17 +59,4 @@ const Container = styled.div`
   justify-content: center;
   gap: 1rem;
   margin: 45px auto 0;
-`;
-
-const TitleArea = styled.section`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 0.3rem;
-  & h1 {
-    font-size: 2rem;
-    font-weight: bold;
-    color: #2f89d1;
-  }
-  margin-bottom: 15px;
 `;
